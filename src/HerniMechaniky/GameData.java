8ec08b5449a0af3724f472d57b61508becf1585b
Honza <jenik.kopac@gmail.com>
2026-02-01 13:52:18 +0100

    pridani nacteni postav

--- src/HerniMechaniky/GameData.java
+++ src/HerniMechaniky/GameData.java
@@ -7,11 +7,13 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import Predmety.Predmety;
+import Postavy.Postavy;
 
 public class GameData {
 
     public ArrayList<Lokace> locations;
     public ArrayList<Predmety> items;
+    public ArrayList<Postavy> characters;
 
     public static GameData loadGameDataFromResources(String resourcePath) {
         Gson gson = new Gson();
